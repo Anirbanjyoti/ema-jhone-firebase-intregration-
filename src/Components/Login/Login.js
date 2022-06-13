1edf775ee4f7c1fee786297df056330e153614cb
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-13 18:06:35 +0530

    User Log in completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import './Login.css';
 
 const Login = () => {
+const [email, setEmail] = useState();
+const [password, setPassword] = useState();
+  // After create user navigate to shop or home page
+  const navigate = useNavigate();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+const handleEmailBlur = event =>{
+  setEmail(event.target.value)
+}
+const handlePasswordBlur = event =>{
+  setPassword(event.target.value)
+}
+  // After LogIn user go to shop page
+  if(user){
+    navigate('/shop')
+  }
+const handleUserSignIn = event=>{
+  event.preventDefault();
+  signInWithEmailAndPassword(email, password)
+}
+
   return (
     <div className="form-container">
-      <Form>
+      <Form onSubmit={handleUserSignIn}>
       <h2 style={{textAlign:'center'}}>Sign In</h2>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
           <br />
-          <Form.Control className="input-field"  type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailBlur} className="input-field"  type="email" placeholder="Enter email" />
           {/* <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text> */}
@@ -20,11 +50,18 @@ const Login = () => {
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <br />
-          <Form.Control className="input-field" type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordBlur} className="input-field" type="password" placeholder="Password" />
         </Form.Group>
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group> */}
+     
+      {
+        loading && <p>Loading....</p>
+      }
+      {/* Firebase Error Message */}
+      <p style={{color:'red'}}>{error?.message}</p>
+       
         <Button  className="log" variant="primary" type="submit">
           Login
         </Button>
